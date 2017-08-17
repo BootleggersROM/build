8d3324c1fe418dddf5edc8cbb495160281364bd6
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add custom build support
    core: Move custom sepolicy to device/
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

--- core/config.mk
+++ core/config.mk
@@ -1089,4 +1089,14 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
+ifneq ($(BOOTLEGGERS_BUILD),)
+## We need to be sure the global selinux policies are included
+## last, to avoid accidental resetting by device configs
+$(eval include device/custom/sepolicy/common/sepolicy.mk)
+
+# Include any vendor specific config.mk file
+include $(TOPDIR)vendor/bootleggers/build/core/config.mk
+
+endif
+
 include $(BUILD_SYSTEM)/dumpvar.mk
