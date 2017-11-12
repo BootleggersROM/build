ac76b1ab4a0fb40729d9b264dfc80e852a5ee021
Ivan Iskandar <iiiiskandar14@gmail.com>
2017-11-12 11:34:48 +0700

    build: Introduce SubstratumService
    Change-Id: I38f44cc25108d2fe5c48dd9b05392f2e0512c832

--- target/product/core.mk
+++ target/product/core.mk
@@ -56,6 +56,7 @@ PRODUCT_PACKAGES += \
     SettingsShishuThemeOverlay \
     SharedStorageBackup \
     StorageManager \
+    SubstratumService \
     Telecom \
     TeleService \
     VpnDialogs \
@@ -67,6 +68,7 @@ PRODUCT_SYSTEM_SERVER_APPS += \
     FusedLocation \
     InputDevices \
     KeyChain \
+    SubstratumService \
     Telecom \
 
 # The set of packages we want to force 'speed' compilation on.
