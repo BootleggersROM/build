e43d06b7cc0d59b1eec3e0ed5fc78bdcf8808b96
ElDainosor <eldainosor@gmail.com>
2017-11-21 03:46:20 +0000

    Revert "Set bootleggers ringtones as defaults"
    This reverts commit 1ac1ebb0f11992f432af72840e53be03681202b4.
    
    This doesn't seems to work. Will attempt to make it different on vendor

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -38,19 +38,15 @@ PRODUCT_PACKAGES += \
     PhotoTable
 
 # Additional settings used in all AOSP builds
-ifeq ($(wildcard vendor/bootleggers/config/common_audio.mk),)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
-endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
-ifeq ($(wildcard vendor/bootleggers/config/common_audio.mk),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-endif
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
